970c6d998cecf80a40624c1aea18b7775628a270
Matthew King <matthew.king@monnsta.net>
2013-08-13 11:08:45 +0100

    Complete rewrite. Needs documenting.

---
+++ lib/atwan/initialiser/autorun.sh
@@ -0,0 +1,11 @@
+#!sh
+# _sourced_ by the initialiser script on the new server.
+#
+# The steps to execute on the new server are appended to this file.
+
+if [ -z "$at_top" ]; then
+  echo at_top is not set. Don\'t know where to look. >&2
+  exit 1
+fi
+
+### HEADER END
